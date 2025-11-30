24f1bcda9141d39732c83ae3aae8e9905c2f446a
k1rvl07 <kirkorol07@gmail.com>
2025-12-01 00:22:23 +0300

    feat: add local fonts and docker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import { Footer, Header } from "@components";
 import { Providers } from "@providers";
 import type { WithChildren } from "@types";
-import classnames from "classnames";
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
 import "antd/dist/reset.css";
 import "@styles";
-
-const roboto = Roboto({
-  weight: ["300", "400", "500", "700"],
-  subsets: ["latin"],
-  variable: "--font-roboto",
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "404chan",
@@ -21,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: WithChildren) {
   return (
-    <html lang="en" className={classnames(roboto.variable)}>
+    <html lang="en">
       <head>
         <link rel="icon" href="/logo.ico" />
       </head>
