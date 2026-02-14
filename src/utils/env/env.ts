bce71eb4462e9ef17420b68a7a147f3f5e3c4a95
k1rvl07 <kirkorol07@gmail.com>
2026-02-14 20:39:34 +0300

    fix: env

--- src/utils/env/env.ts
+++ src/utils/env/env.ts
@@ -1,8 +1,6 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_MINIO_URL: string;
-      NEXT_PUBLIC_MINIO_BUCKET_URL: string;
       NEXT_PUBLIC_WS_URL: string;
       NEXT_PUBLIC_API_URL: string;
       NEXT_PUBLIC_ENV: "development" | "production";
@@ -11,8 +9,6 @@ declare global {
 }
 
 export const env = {
-  MINIO_URL: process.env.NEXT_PUBLIC_MINIO_URL,
-  MINIO_BUCKET_URL: process.env.NEXT_PUBLIC_MINIO_BUCKET_URL,
   WS_URL: process.env.NEXT_PUBLIC_WS_URL,
   API_URL: process.env.NEXT_PUBLIC_API_URL,
   ENV: process.env.NEXT_PUBLIC_ENV,
